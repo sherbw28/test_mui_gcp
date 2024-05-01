da1b6d75c4adc2ce2ee2a3028326216661b1db23
Eisuke Sato <sherbw28@outlook.jp>
2024-05-01 12:55:41 +0900

    prittier,eslint設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,31 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Box, Button, Divider, Grid, Input, Stack, Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Stack height="300px" justifyContent="center" alignItems="center" gap="32px" bgcolor="green">
+        <Typography id="login_heading" variant="h1" fontSize="1.5rem">
+          ログインフォーム
+        </Typography>
+        <Stack
+          direction="row"
+          bgcolor="pink"
+          divider={<Divider orientation="vertical" flexItem />}
+          width="50%"
+          justifyContent="space-between"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Input placeholder="メールアドレス" />
+          <Input placeholder="ユーザーネーム" />
+          <Button variant="contained">ログイン</Button>
+        </Stack>
+      </Stack>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} bgcolor="blue">
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid xs={2} sm={4} md={4} key={index}>
+            <Box>xs=2</Box>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
